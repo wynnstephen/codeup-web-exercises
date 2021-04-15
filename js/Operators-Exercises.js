f836d363c97b49e15b10f3ce5b3714e344470ad2
stephen <stephen.h.nguyen.19@gmail.com>
2021-04-15 16:18:43 -0500

    This is my Operators-Exercises.js

---
+++ js/Operators-Exercises.js
@@ -0,0 +1,34 @@
+"use script";
+
+console.log('1' + 2);// = 12
+
+console.log(typeof ('1' + 2));
+
+console.log(6 % 4);
+
+console.log('3 + 4 is ' + 3 +4);
+
+console.log(0 <0);
+
+console.log('false' ==false);
+
+console.log(true == 'true');
+
+console.log(5 >= -5);
+
+console.log(!false || false);
+
+console.log(6 % 5);
+
+console.log(5 < 4 && 1 === 1);
+
+console.log(typeof ('codeup' === 'string'))
+
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+
+console.log(4>= 0 && 1 !== '1');
+
+console.log(6 % 3 ===0);
+
+console.log(5 % 2 !== 0);
+
