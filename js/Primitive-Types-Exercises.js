9fa0082159c3f9684dda965ed6ef2036d4af1ebc
stephen <stephen.h.nguyen.19@gmail.com>
2021-04-15 15:33:00 -0500

    This is my Primitive-Types-Exercises.js

---
+++ js/Primitive-Types-Exercises.js
@@ -0,0 +1,23 @@
+"use script";
+
+// 99.9 is a number
+// "false" is a boolean
+// false is a boolean
+// '0' is a number
+// 0 is a number
+// true is a boolean
+// 'true' is a boolean
+
+//----------------------------------------
+// Q: A term or a phrase typed into a search bar?
+// A: A string
+// Q: If a user is logged in?
+// A: a string
+// Q: A discount amount to apply to a user's shopping  cart?
+// A: a number
+// Q: Whether or not a coupon code is valid?
+// A: a boolean
+// Q: An email address typed into a registration form?
+// A: a string
+// Q: The price of a product
+// A: a number
