baee5dd428c03c79d5297e61d9562cf6bfe6ee49
stephen <stephen.h.nguyen.19@gmail.com>
2021-04-19 11:07:17 -0500

    adds DOM-lecture.html

---
+++ js/DOM-lecture.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// TODO: print the following items to the console: 1) A node that
+//  represents a div/item with a red border. 2) an array of elements
+//  whose text is dark-green. 3) an array of all paragraph elements
+
+function (){
+
+}
