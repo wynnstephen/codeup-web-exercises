56696707c10365fd069146047659c7d00ab7e3ff
stephen <stephen.h.nguyen.19@gmail.com>
2021-04-27 16:46:24 -0500

    adds assessment-two-review.js

---
+++ js/assessment-two-review.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+/**
+* Create a function called 'isTwo' that takes a number as a parameter.
+* The function should return a boolean value based on whether or not the passed
+* number is the number 2.
+*
+* Example
+* > isTwo(1) // returns false
+* > isTwo(2) // returns true
+* > isTwo(3) // returns false
+*
+* Call the function 'isTwo' passing the variable 'random' as a argument.
+*
+* console.log *outside of the function* to check your work (you should see a
+* different result everytime you refresh the page if you are using the random
+* number)
+*/
+
+function isTwo(num){
+
+}
