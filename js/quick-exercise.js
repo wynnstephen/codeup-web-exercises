f4a29fe7a7d3a2a76f28f83db3b09137f7b1993f
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-03 09:28:17 -0500

    add quick morning exercise

---
+++ js/quick-exercise.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Make a function named trim(string) that removes empty spaces before and after the input.
+
+function trim(input){
+    return  input.replaceAll(' ', '');
+
+}
+
+console.log(trim(' string '));
+
