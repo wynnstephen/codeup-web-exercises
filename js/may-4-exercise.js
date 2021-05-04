08a5ee81912f53b0c9e6b44fb3e58d8ac06d4cf9
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-04 12:25:41 -0500

    adds css lecture exercise

---
+++ js/may-4-exercise.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Write a function named cube(n) that returns n * n * n
+// Write a function named cubeRoot(n) that returns the cube root of the input
+
+function cube(n){
+    return cube(n/n/m);
+}
+
+console.log(cube(n/n/n));
