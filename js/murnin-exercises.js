510324700454d15737eed827f9d15be3e522883a
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-05 09:50:52 -0500

    adds murnin-exercises.js

---
+++ js/murnin-exercises.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// Write a function named cube(n) that returns n * n * n
+// Write a function named cubeRoot(n) that returns the cube root of the input
+
+// function cube(n){
+//     return cube(n/n/m);
+// }
+//
+// console.log(cube(n/n/n));
+
+// Write a function called last(input) that returns the last character of a string
+
+function last(input){
+    return input.slice(-1);
+}
+
+console.log(last("Shots?"))
