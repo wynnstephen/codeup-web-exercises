6d4b5e6b00bcad460297379640133b0f3e6d5904
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-06 10:09:32 -0500

    adds css exercises

--- js/murnin-exercises.js
+++ js/murnin-exercises.js
@@ -11,8 +11,18 @@
 
 // Write a function called last(input) that returns the last character of a string
 
-function last(input){
-    return input.slice(-1);
+// function last(input){
+//     return input.slice(-1);
+// }
+//
+// console.log(last("Shots?"))
+
+// Write a remainder(number, divisor) function that returns the remainder left over when dividing number by the divisor
+// If either of the arguments are not a numeric string or number, return a string message indicating which argument is not a number/numeric string
+
+
+function remainder(x){
+    return remainder % x;
 }
 
-console.log(last("Shots?"))
+console.log(remainder(4))
