3b9690fc38e1719bedaaf3ce656750a535ea14f8
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-19 11:11:37 -0500

    adds DOM-lecture.js

--- js/DOM-lecture.js
+++ js/DOM-lecture.js
@@ -4,6 +4,15 @@
 //  represents a div/item with a red border. 2) an array of elements
 //  whose text is dark-green. 3) an array of all paragraph elements
 
-function (){
 
-}
+//1.
+let redBrd = document.getElementById("red-border");
+console.log(redBrd);
+
+//2.
+let drkGrn = document.getElementsByClassName('drk-grn');
+console.log(drkGrn);
+
+//3.
+let allPars = document.getElementsByTagName('p');
+console.log(allPars)
