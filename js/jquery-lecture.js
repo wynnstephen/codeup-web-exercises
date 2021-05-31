a7b288729ee30ad16524f3ab606a7d896514a5a3
stephen <stephen.h.nguyen.19@gmail.com>
2021-05-31 10:01:36 -0500

    adds notes to jquery.lecture

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -1,2 +1,17 @@
 "use strict";
 
+// syntax
+// let document = {
+//     getElementById: function (){
+//         console.log("find element with id of X")
+//     }
+// }
+// document.getElementById()
+
+
+// dollar sign - tapping into jQuery library
+// selector - the element that you're selecting from the dom
+//$("selector").methods)any arguments required);
+
+//syntax - select by tag Name
+$("h1")//... --> select by tagName
