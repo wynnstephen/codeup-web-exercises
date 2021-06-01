ed2b7036482aa156f5ad96f0cb4f5f30944383fb
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-01 14:44:29 -0500

    adds lecture exercises

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -13,15 +13,55 @@
 // selector - the element that you're selecting from the dom
 //$("selector").methods)any arguments required);
 
-//syntax - select by tag Name
-$("h1")//... --> select by tagName
-$("span").hide()
+// //syntax - select by tag Name
+// $("h1")//... --> select by tagName
+// $("span").hide()
+//
+// //syntax - selecting by id. Similar to selecting id in CSS. use --> #
+// $("#change-text")
+// $("#all-caps").hide()
+//
+// //syntax - selecting by class. Similar to selecting class in CSS. use --> .
+// $(".lead")
+// $(".show-me").show()
+// //selecting children/parent/sibling elements
 
-//syntax - selecting by id. Similar to selecting id in CSS. use --> #
-$("#change-text")
-$("#all-caps").hide()
+//TODO: add 3 images to your HTML page. give the element a pink background using jQuery. give the element an orange background using jQuery.
+// Once the window has finished loading, hide the div with a pink background color. Once the DOM has finished loading hide the div with the orange background color.
 
-//syntax - selecting by class. Similar to selecting class in CSS. use --> .
-$(".lead")
-$(".show-me").show()
-//selecting children/parent/sibling elements
+$('img').css('width', '30%');
+//$(`img`).css('background', 'pink');
+
+// $('.pink').hide()
+// $('.orange')
+
+$('.pink').css('background', 'pink');
+$('.orange').css('background', 'orange');
+
+$('#p').hide()
+$("button").click(function (){
+    $("#o").load("demo_test.txt")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
