36f92e7b8df15c2b9fe1fc6b5b647edf067da03c
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-01 12:01:33 -0500

    adds notes to jquery-lecture

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -15,9 +15,13 @@
 
 //syntax - select by tag Name
 $("h1")//... --> select by tagName
+$("span").hide()
 
 //syntax - selecting by id. Similar to selecting id in CSS. use --> #
 $("#change-text")
+$("#all-caps").hide()
 
 //syntax - selecting by class. Similar to selecting class in CSS. use --> .
 $(".lead")
+$(".show-me").show()
+//selecting children/parent/sibling elements
