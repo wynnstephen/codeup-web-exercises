fb1c3742422f47b03e29871af2cbadc465621cc4
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-04 10:42:32 -0500

    adds jquery_exercise

--- jQuery/jquery_exercises.js
+++ jQuery/jquery_exercises.js
@@ -1,2 +1,18 @@
 "use strict";
 
+$("#lagoon").click(function (){
+    $('#lagoon').css('background-color', '#FF0');
+})
+
+$('p').dblclick(function (){
+    $('p').css('font-size', '18px');
+})
+
+$('li').hover(
+    function () {
+        $('li').css('background-color', 'red');
+    },
+    function () {
+        $('li').css('background-color', 'black');
+    }
+);
