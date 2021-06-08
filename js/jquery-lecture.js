bbe7879db2a271036c60ef84602a680204ca60e9
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-08 11:45:59 -0500

    adds ajax lecture

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -29,7 +29,7 @@
 //TODO: add 3 images to your HTML page. give the element a pink background using jQuery. give the element an orange background using jQuery.
 // Once the window has finished loading, hide the div with a pink background color. Once the DOM has finished loading hide the div with the orange background color.
 
-$('img').css('width', '30%');
+$('img').css('width', '1000%');
 //$(`img`).css('background', 'pink');
 
 // $('.pink').hide()
