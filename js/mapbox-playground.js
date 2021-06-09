07f11e6506831d548261ee8a37f4328a4462592a
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-09 10:58:06 -0500

    adds mapbox-playground

---
+++ js/mapbox-playground.js
@@ -0,0 +1,6 @@
+const mapboxToken = 'pk.eyJ1Ijoid3lubnN0ZXBoZW4iLCJhIjoiY2twcG44ZGJiMDJtbTJ1cGZiOXFlbHQxaSJ9.5OtxyKlsQ8leAWW4EQnmnA'
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/'
+})
