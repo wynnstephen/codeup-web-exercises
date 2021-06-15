042ce78e0b72e659f00efc1e792ca97b0ad1957f
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-15 11:58:54 -0500

    adds map-filter-reduce.js

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,88 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const users = [...]
+
+// let userWithLanguages = user.filter(function (user){
+//     return user.languages.length >= 3;
+// })
+//
+// console.log(userWithLanguages);
+
+// let emails = users.map(user => user.email);
+//
+// console.log(emails);
+
+// let averageYrs = users.reduce((accumulator, currentVal) =>{
+//     return accumulator + currentVal.yearsOfExperience;
+// }, 0) / users.length;
+//
+// console.log(averageYrs);
+
+// let emails = users.map(user => user.email);
+//
+// console.log(emails);
+//
+// let longestEmail = emails.reduce((accumulator, currentValue){
+//     // return accumulator.length > currentValue.length
+//     // ? accumulator
+//     //     : currentValue;
+//
+//     if (accumulator.lenght > currentValue.length){
+//         return accumulator;
+//     }
+//     return currentValue
+// }, "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
