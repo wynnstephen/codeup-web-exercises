3f79d8b044df3679396fdf3ba15e84051eacb615
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-20 23:30:51 -0500

    adds konami.html

--- js/murnin-exercises.js
+++ js/murnin-exercises.js
@@ -40,6 +40,34 @@
 // Write a function named secondToLast(arr) that returns the data type of the second to last element of the array parameter.
 //     When you call the function, pass in an array of your choosing
 
-function secondToLast(arr){
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.map(n => n * 3)
+// console.log(numbers)
 
-}
+// if (true) {
+//     let x = 5;
+// }
+// console.log(typeof x);
+
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+// console.log(numbers);
+
+// function sayHello(name) {
+//     return 'Hello, ' + name + '!';
+// }
+
+// const sayHello = (name) => { 'Hello, ' + name + '!' }
+
+// const sayHello = name => { 'Hello, ' + name + '!' }
+
+// const sayHello = (name) => `Hello, ${name}!`
+//
+// console.log(sayHello())
+
+var myOutput = originalString + " is an example of " + description + " thinking.";
+
+console.log(orginalString)
